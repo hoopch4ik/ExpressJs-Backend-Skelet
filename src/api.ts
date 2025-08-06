7324152c8d80e8099dc2726d9c1388a59d26f932
Nikita <alenavolkova575@gmail.com>
2025-08-07 02:07:04 +0300

    change in api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
-import { seregaRouter } from '@routes/seregaRouter';
-import { siteRouter } from '@routes/siteRouter';
-import express from 'express';
+import { seregaRouter } from '@routes/seregaRouter';import express from 'express';
+
+
 
 // Import routes from the ./routes
 // import auth from "@routes/authRoute";
@@ -10,7 +10,6 @@ const api = express.Router();
 
 // Setup routing
 api.use("/", siteRouter);
-api.use("/serega", seregaRouter);
 
 
 export {api as apiv1};
